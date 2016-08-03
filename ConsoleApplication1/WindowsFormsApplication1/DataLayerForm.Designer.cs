2d3ecbeaea76f3af5c46d3fc58d6af3209f248d5
Marchello700 <mantas.taroza@gmail.com>
2016-08-03 10:59:50 +0300

    added DataMetricsFormProject to solution

---
+++ ConsoleApplication1/WindowsFormsApplication1/DataLayerForm.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class DataLayerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.CpuTextBox = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.UserLabel = new System.Windows.Forms.Label();
+            this.CpuLabel = new System.Windows.Forms.Label();
+            this.RamLabel = new System.Windows.Forms.Label();
+            this.RamTextBox = new System.Windows.Forms.TextBox();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.UserTextBox = new System.Windows.Forms.TextBox();
+            this.VideoCardTexBox = new System.Windows.Forms.TextBox();
+            this.VideoCardLabel = new System.Windows.Forms.Label();
+            this.IpTextBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.IpLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CpuTextBox
+            // 
+            this.CpuTextBox.Location = new System.Drawing.Point(139, 72);
+            this.CpuTextBox.Name = "CpuTextBox";
+            this.CpuTextBox.Size = new System.Drawing.Size(342, 20);
+            this.CpuTextBox.TabIndex = 1;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(71, 20);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(35, 13);
+            this.NameLabel.TabIndex = 2;
+            this.NameLabel.Text = "label1";
+            // 
+            // UserLabel
+            // 
+            this.UserLabel.AutoSize = true;
+            this.UserLabel.Location = new System.Drawing.Point(71, 46);
+            this.UserLabel.Name = "UserLabel";
+            this.UserLabel.Size = new System.Drawing.Size(35, 13);
+            this.UserLabel.TabIndex = 3;
+            this.UserLabel.Text = "label2";
+            // 
+            // CpuLabel
+            // 
+            this.CpuLabel.AutoSize = true;
+            this.CpuLabel.Location = new System.Drawing.Point(71, 72);
+            this.CpuLabel.Name = "CpuLabel";
+            this.CpuLabel.Size = new System.Drawing.Size(35, 13);
+            this.CpuLabel.TabIndex = 4;
+            this.CpuLabel.Text = "label3";
+            // 
+            // RamLabel
+            // 
+            this.RamLabel.AutoSize = true;
+            this.RamLabel.Location = new System.Drawing.Point(71, 98);
+            this.RamLabel.Name = "RamLabel";
+            this.RamLabel.Size = new System.Drawing.Size(35, 13);
+            this.RamLabel.TabIndex = 5;
+            this.RamLabel.Text = "label4";
+            // 
+            // RamTextBox
+            // 
+            this.RamTextBox.Location = new System.Drawing.Point(139, 98);
+            this.RamTextBox.Name = "RamTextBox";
+            this.RamTextBox.Size = new System.Drawing.Size(342, 20);
+            this.RamTextBox.TabIndex = 6;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(139, 20);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(342, 20);
+            this.NameTextBox.TabIndex = 7;
+            // 
+            // UserTextBox
+            // 
+            this.UserTextBox.Location = new System.Drawing.Point(139, 46);
+            this.UserTextBox.Name = "UserTextBox";
+            this.UserTextBox.Size = new System.Drawing.Size(342, 20);
+            this.UserTextBox.TabIndex = 8;
+            // 
+            // VideoCardTexBox
+            // 
+            this.VideoCardTexBox.Location = new System.Drawing.Point(139, 124);
+            this.VideoCardTexBox.Name = "VideoCardTexBox";
+            this.VideoCardTexBox.Size = new System.Drawing.Size(342, 20);
+            this.VideoCardTexBox.TabIndex = 9;
+            // 
+            // VideoCardLabel
+            // 
+            this.VideoCardLabel.AutoSize = true;
+            this.VideoCardLabel.Location = new System.Drawing.Point(71, 124);
+            this.VideoCardLabel.Name = "VideoCardLabel";
+            this.VideoCardLabel.Size = new System.Drawing.Size(35, 13);
+            this.VideoCardLabel.TabIndex = 10;
+            this.VideoCardLabel.Text = "label5";
+            // 
+            // IpTextBox
+            // 
+            this.IpTextBox.Location = new System.Drawing.Point(139, 150);
+            this.IpTextBox.Name = "IpTextBox";
+            this.IpTextBox.Size = new System.Drawing.Size(342, 20);
+            this.IpTextBox.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(139, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(342, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // IpLabel
+            // 
+            this.IpLabel.AutoSize = true;
+            this.IpLabel.Location = new System.Drawing.Point(71, 150);
+            this.IpLabel.Name = "IpLabel";
+            this.IpLabel.Size = new System.Drawing.Size(35, 13);
+            this.IpLabel.TabIndex = 13;
+            this.IpLabel.Text = "label5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "label5";
+            // 
+            // DataLayerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 372);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.IpLabel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.IpTextBox);
+            this.Controls.Add(this.VideoCardLabel);
+            this.Controls.Add(this.VideoCardTexBox);
+            this.Controls.Add(this.UserTextBox);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.RamTextBox);
+            this.Controls.Add(this.RamLabel);
+            this.Controls.Add(this.CpuLabel);
+            this.Controls.Add(this.UserLabel);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.CpuTextBox);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "DataLayerForm";
+            this.Text = "DataLayerForm";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.DataLayerForm_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox CpuTextBox;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label UserLabel;
+        private System.Windows.Forms.Label CpuLabel;
+        private System.Windows.Forms.Label RamLabel;
+        private System.Windows.Forms.TextBox RamTextBox;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.TextBox UserTextBox;
+        private System.Windows.Forms.TextBox VideoCardTexBox;
+        private System.Windows.Forms.Label VideoCardLabel;
+        private System.Windows.Forms.TextBox IpTextBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label IpLabel;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ConsoleApplication1/WindowsFormsApplication1/DataLayerForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+using DataLayer;
+
+namespace WindowsFormsApplication1
+{
+    public partial class DataLayerForm : Form
+    {
+        private FullDataManager dataManager;
+
+        private void InitializeLabels()
+        {
+            NameLabel.Text = "Name";
+            UserLabel.Text = "User";
+            CpuLabel.Text = "CPU";
+            RamLabel.Text = "RAM";
+            VideoCardLabel.Text = "Video Card";
+            IpLabel.Text = "IP";
+        }
+
+        private void InitializeTextBoxes()
+        {
+            NameTextBox.Text = dataManager.GetName();
+            UserTextBox.Text = dataManager.GetUser();
+            CpuTextBox.Text = dataManager.GetCpu();
+            RamTextBox.Text = $"Ram: {dataManager.GetRamGb()}GB";
+            VideoCardTexBox.Text = dataManager.GetVideoCard();
+            IpTextBox.Text = $"Ip Adress: {dataManager.GetIp()}";
+        }
+
+        public DataLayerForm()
+        {
+            InitializeComponent();
+            dataManager = new FullDataManager();
+            InitializeLabels();
+            InitializeTextBoxes();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InitializeLabels();
+            InitializeTextBoxes();
+        }
+
+        private void DataLayerForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Close();
+            }
+        }
+    }
+}
